3600251beba7a90f33ec2344b11fe1525bedeed2
Venkata Ramana Reddy K <mekvrreddy@gmail.com>
2023-06-28 17:55:00 +0530

    optimised the CodeToRefactor and also added unit tests

--- ReFactor/CodeToRefactor.cs
+++ ReFactor/CodeToRefactor.cs
@@ -4,16 +4,34 @@ using System.Linq;
 
 namespace CodingAssessment.Refactor
 {
+    /// <summary>
+    /// refernce type used to hold person specific details like Name and DOB
+    /// </summary>
     public class People
     {
         private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
+
+        /// <summary>
+        /// Name of the person
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// DOB of the person
+        /// </summary>
         public DateTimeOffset DOB { get; private set; }
 
+        /// <summary>
+        /// creating people object for the given name
+        /// </summary>
+        /// <param name="name">name</param>
         public People(string name) : this(name, Under16.Date)
         {
         }
-
+        /// <summary>
+        /// creating people object for the given name and DOB 
+        /// </summary>
+        /// <param name="name">person name</param>
+        /// <param name="dob">person date of birth</param>
         public People(string name, DateTime dob)
         {
             Name = name;
@@ -21,6 +39,9 @@ namespace CodingAssessment.Refactor
         }
     }
 
+    /// <summary>
+    /// Contains methods related persons
+    /// </summary>
     public class BirthingUnit
     {
         /// <summary>
@@ -34,52 +55,70 @@ namespace CodingAssessment.Refactor
         }
 
         /// <summary>
-        /// GetPeoples
+        /// Retrive Top/Bottom j peoples
         /// </summary>
-        /// <param name="j"></param>
-        /// <returns>List<object></returns>
+        /// <param name="j">Top/Bottom j items</param>
+        /// <returns>collection of People type</returns>
         public List<People> GetPeople(int i)
         {
+            var random = new Random();
             for (int j = 0; j < i; j++)
             {
                 try
                 {
                     // Creates a dandon Name
                     string name = string.Empty;
-                    var random = new Random();
-                    if (random.Next(0, 1) == 0) {
-                        name = "Bob";
-                    }
-                    else {
-                        name = "Betty";
-                    }
+                    name = random.Next(0, 1) == 0 ? "Bob" : "Betty";                   
                     // Adds new people to the list
                     _people.Add(new People(name, DateTime.UtcNow.Subtract(new TimeSpan(random.Next(18, 85) * 356, 0, 0, 0))));
                 }
                 catch (Exception e)
                 {
                     // Dont think this should ever happen
-                    throw new Exception("Something failed in user creation");
+                    throw new Exception(ApplicationConstants.ERROR_MESSAGE);
                 }
             }
             return _people;
         }
 
-        private IEnumerable<People> GetBobs(bool olderThan30)
+        /// <summary>
+        /// return collection of persons whose age is greater than 30
+        /// </summary>
+        /// <param name="name">person name</param>
+        /// <param name="olderThan30"></param>
+        /// <returns></returns>
+        private IEnumerable<People> GetNames(string name,bool olderThan30)
         {
-            return olderThan30 ? _people.Where(x => x.Name == "Bob" && x.DOB >= DateTime.Now.Subtract(new TimeSpan(30 * 356, 0, 0, 0))) : _people.Where(x => x.Name == "Bob");
+            return olderThan30 ? _people.Where(x => x.Name == name && x.DOB >= DateTime.Now.Subtract(new TimeSpan(30 * 356, 0, 0, 0))) : _people.Where(x => x.Name == name);
         }
 
+        /// <summary>
+        /// fetch the person new name after appending last name to the existing name
+        /// </summary>
+        /// <param name="p">person</param>
+        /// <param name="lastName">person last name</param>
+        /// <returns>person new name after appending last name</returns>
         public string GetMarried(People p, string lastName)
         {
-            if (lastName.Contains("test"))
+            //null check
+            if (lastName.Contains(ApplicationConstants.DEFAULT_PERSON_LASTNAME))
                 return p.Name;
-            if ((p.Name.Length + lastName).Length > 255)
+            if ((p.Name.Length + lastName).Length > ApplicationConstants.PERSON_NAME_LIMIT_LENGTH)
             {
-                (p.Name + " " + lastName).Substring(0, 255);
+                (p.Name + " " + lastName).Substring(0, ApplicationConstants.PERSON_NAME_LIMIT_LENGTH);
             }
 
             return p.Name + " " + lastName;
         }
+    }
+
+    /// <summary>
+    /// Application specific constants
+    /// </summary>
+    public class ApplicationConstants
+    {
+        public const string ERROR_MESSAGE = "Something failed in user creation";
+        public const string DEFAULT_PERSON_LASTNAME = "test";
+        public const int PERSON_NAME_LIMIT_LENGTH = 255;
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using CodingAssessment.Refactor;
 using FluentAssertions;
 using Xunit;
 
@@ -6,10 +8,28 @@ namespace Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(-1, 0)]
+        [InlineData(-2, 0)]
+        [InlineData(-3, 0)]
+        [InlineData(-10, 0)]
+        public void GetPeople_ReturnNoRowsForNegativeValues(int input, int expectedCollectionCount)
         {
-            true.Should().BeTrue();
+            BirthingUnit irthingUnit = new BirthingUnit();
+            List<People> items = irthingUnit.GetPeople(input);
+            Assert.Equal(items.Count, expectedCollectionCount);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(10, 10)]
+        public void GetPeople_ReturnCollectionForPositiveInputValue(int input, int expectedCollectionCount)
+        {
+            BirthingUnit irthingUnit = new BirthingUnit();
+            List<People> items = irthingUnit.GetPeople(input);
+            Assert.Equal(items.Count, expectedCollectionCount);
         }
     }
 }
